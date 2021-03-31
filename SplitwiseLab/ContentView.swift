83fd768336b1106f2a6204353b85bdf231c28091
joferava <jfelipe221095@gmail.com>
2021-03-30 20:17:06 -0600

    Initial Commit

---
+++ SplitwiseLab/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SplitwiseLab
+//
+//  Created by Felipe Ramirez Vargas on 30/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SplitwiseLab/SplitwiseLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitwiseLabApp.swift
+//  SplitwiseLab
+//
+//  Created by Felipe Ramirez Vargas on 30/3/21.
+//
+
+import SwiftUI
+
+@main
+struct SplitwiseLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
